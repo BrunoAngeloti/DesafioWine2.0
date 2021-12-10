739ccf6a2adf52b37ee58d9a1b90342890f35a0d
Bruno Angeloti <69606747+BrunoAngeloti@users.noreply.github.com>
2021-12-10 00:58:56 -0300

    tentando corrigir o erro de deploy

--- store/ducks/items/sagas.ts
+++ store/ducks/items/sagas.ts
@@ -3,16 +3,7 @@ import { call, put } from 'redux-saga/effects'
 import api from '../../../services/api'
 import { PriceFilterState } from '../pricesFilter/types'
 
-interface iRequestPayload{
-    filter: PriceFilterState,
-    pageAtual: number
-}
-
-interface iRequestWine{
-    payload: iRequestPayload
-}
-
-export function* loadingWines({payload}: iRequestWine): any{  
+export function* loadingWines({payload}:any): any{  
     const { filter, pageAtual } = payload
     const response = yield call(api.get, 
         `products?page=${pageAtual}&limit=10&filter=${filter.min}-${filter.max}`
