0d750ddba886d4f6e3eb3a5d5381ed611d73401a
Bruno Angeloti <69606747+BrunoAngeloti@users.noreply.github.com>
2021-12-10 00:48:12 -0300

    Arrumando possivel erro

--- store/ducks/items/types.ts
+++ store/ducks/items/types.ts
@@ -1,11 +1,11 @@
 import { iWines } from "../../../interfaces/wines";
 
 export enum ItemsTypes{
-    SET_ITEMS = '@amountItems/SET_ITEMS',
-    REQUEST_ITEMS = '@amountItems/REQUEST_ITEMS',
-    REQUEST_LOADING_ITEMS = '@amountItems/REQUEST_LOADING_ITEMS',
-    REQUEST_SUCCESS_ITEMS = '@amountItems/REQUEST_SUCCESS_ITEMS',
-    REQUEST_FAILED_ITEMS = '@amountItems/REQUEST_FAILED_ITEMS',
+    SET_ITEMS = '@items/SET_ITEMS',
+    REQUEST_ITEMS = '@items/REQUEST_ITEMS',
+    REQUEST_LOADING_ITEMS = '@items/REQUEST_LOADING_ITEMS',
+    REQUEST_SUCCESS_ITEMS = '@items/REQUEST_SUCCESS_ITEMS',
+    REQUEST_FAILED_ITEMS = '@items/REQUEST_FAILED_ITEMS',
 }
 
 export interface ItemsState{
